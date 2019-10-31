6b0b067fb48f9120c474c9f012c3793c22df8c1d
maadcock <michael.l.adcock@gmail.com>
2019-10-31 16:58:07 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  class App extends React.Component {
+  state = {
+    count: 0,
+    fname: '',
+    lname: ''
+  }
+
+  constructor(props){
+    super(props);
+    this.updateFName = this.updateFName.bind(this);
+    this.updateLName = this.updateLName.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  updateFName(event){
+    this.setState({fname : event.target.value})
+  }
+
+  updateLName(event){
+    this.setState({lname : event.target.value})
+  }
+
+  handleSubmit(){
+    this.setState({name : this.state.fname + " " + this.state.lname})
+  } 
+
+  render() {
+    return (
+      <div className="App">
+          <header className="App-header">
+          <form>
+            <input type="text" onChange={this.updateFName}></input><br />
+            <input type="text" onChange={this.updateLName}></input><br />
+            <input type="submit" onClick={this.handleSubmit} ></input>
+          </form>
+          <h1>
+            {this.state.name}
+          </h1>
+        </header>
+      </div>
+    )
+  };
 }
 
 export default App;
